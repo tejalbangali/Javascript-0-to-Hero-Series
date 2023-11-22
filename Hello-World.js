a699c39500d4b85c8b3c400228d8db21a461fced
Tejal Bangali <tejal.bangali@gmail.com>
2023-11-22 23:20:40 +0530

    added 1st question

---
+++ Hello-World.js
@@ -0,0 +1,13 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+// Example 1:
+
+// Input: args = []
+// Output: "Hello World"
+// Explanation:
+// const f = createHelloWorld();
+// f(); // "Hello World"
+
+
+function createHelloWorld() {
+    return (() => "Hello World");
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Javascript-0-to-Hero-Series
+
+### Description - 
+
+Welcome aboard fellow developers, this is where you can find the javascript code for the learning series. 
+So, this is Leet Code questions from easy to levels going upwards. 
+
+## Have fun learning !!
